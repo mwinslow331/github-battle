89a81c67d3e63eea80cec5b5751d043c26f80fad
mwinslow331 <michael.winslow331@gmail.com>
2018-03-14 15:34:03 -0400

    languages list created

--- app/App.js
+++ app/App.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-require('./index.css');
+import Popular from './components/Popular';
 
 class App extends Component{
   render(){
     return(
-      <div>
-        Hello React Training!
+      <div className='container'>
+        <Popular />
       </div>
     )
   }
