380af42b976a499c66ff428fd0ba057fcda09ad0
mwinslow331 <michael.winslow331@gmail.com>
2018-03-14 14:40:42 -0400

    first commit

---
+++ app/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+require('./index.css');
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        Hello React Training!
+      </div>
+    )
+  }
+}
+
+export default App;
