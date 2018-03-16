9d835c7c3b909a4a55e0ea0ad48e36cf428a49c9
mwinslow331 <michael.winslow331@gmail.com>
2018-03-16 13:03:46 -0400

    added routes through react-router

--- app/App.js
+++ app/App.js
@@ -1,13 +1,28 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Popular from './components/Popular';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Nav from './components/Nav';
+import Home from './components/Home';
+import Battle from './components/Battle';
+
 
 class App extends Component{
   render(){
     return(
-      <div className='container'>
-        <Popular />
-      </div>
+      <Router>
+        <div className='container'>
+          <Nav />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/popular' component={Popular} />
+            <Route render={function () {
+              return <p>Not Found</p>
+            }} />
+          </Switch>
+        </div>
+      </Router>
     )
   }
 }
---
+++ app/components/Home.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class Home extends Component {
+  render(){
+    return(
+      <div className='home-container'>
+        <h1>Battle Your Friends and...Stuff</h1>
+
+        <Link className='button' to='/battle'>Battle</Link>
+      </div>
+    )
+  }
+}
+
+export default Home;
