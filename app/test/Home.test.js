8ae009303051f588c14b2c4966f785d4b5ad8885
mwinslow331 <michael.winslow331@gmail.com>
2018-04-24 18:37:27 -0400

    added snapshot test for Nav in App Component

--- app/test/Home.test.js
+++ app/test/Home.test.js
@@ -5,10 +5,18 @@ import { Link } from 'react-router-dom';
 import { StaticRouter, MemoryRouter } from 'react-router'
 import renderer from 'react-test-renderer';
 
-it('renders battle friends title', () => {
-  const wrapper = shallow(<Home />);
-  const welcome = <h1>Battle Your Friends and...Stuff</h1>;
-  expect(wrapper.contains(welcome)).toEqual(true);
+describe('<Home />', () => { 
+  it('renders battle friends title', () => {
+    const wrapper = shallow(<Home />);
+    const welcome = <h1>Battle Your Friends and...Stuff</h1>;
+    expect(wrapper.contains(welcome)).toEqual(true);
+  });
+
+  it('renders a Battle link', () => {
+    const wrapper = shallow(<Home />);
+    const battle = <Link className='button' to='/battle'>Battle</Link>
+    expect(wrapper.contains(battle)).toEqual(true);
+  });
 });
 
 // test('cursor should change when hovering over Battle link', () => {
